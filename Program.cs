c20852acdf03025245ffbd95a0e7b53f8263a29d
Juawo <joaopedrojp2703@gmail.com>
2026-03-24 15:14:02 -0300

    Implement reservas endpoint and add eventos retrieval functionality

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,11 +11,39 @@ builder.Services.AddNpgsqlDataSource(
 
 var app = builder.Build();
 
-app.MapGet("/ping", async (NpgsqlDataSource db) =>
+app.MapPost("/reservas", async (ReservaRequest request, NpgsqlDataSource db) =>
 {
-    await using var conn = await db.OpenConnectionAsync();
-    Console.WriteLine("Pong");
-    return "Ok";
+    await using var connection = db.OpenConnection();
+    var command = new NpgsqlCommand(@"
+        UPDATE eventos SET ingressos_disponiveis = ingressos_disponiveis - 1 
+        WHERE id = @EventoId AND ingressos_disponiveis > 0 RETURNING ingressos_disponiveis;
+   ", connection);
+    command.Parameters.AddWithValue("EventoId", request.EventoId);
+    var result = await command.ExecuteScalarAsync();
+    return result == null ? Results.UnprocessableEntity() : Results.Created();
+});
+
+app.MapGet("/eventos", async (NpgsqlDataSource db) =>
+{
+    await using var connection = db.OpenConnection();
+    var command = new NpgsqlCommand("SELECT * FROM eventos;", connection);
+    var reader = await command.ExecuteReaderAsync();
+    var eventos = new List<Object>();
+    while (await reader.ReadAsync())
+    {
+        eventos.Add(new
+        {
+            id = reader.GetInt16(0),
+            nome = reader.GetString(1),
+            ingressos_disponiveis = reader.GetInt16(2)
+            
+        });
+    }
+    
+    return Results.Ok(eventos);
 });
 
 app.Run("http://0.0.0.0:8080");
+
+
+public record ReservaRequest(int EventoId, int UsuarioId);
